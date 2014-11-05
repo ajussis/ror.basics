82016eddbe08d552bc36be133bc2345a8933bc28
Jussi Savolainen <jussi@concept.zone>
2014-11-06 00:08:25 +0100

    Test bug fix

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Sample application"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
